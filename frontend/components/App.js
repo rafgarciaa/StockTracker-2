2f52dda9b4b627035ebb5f88526b5de67f023f4c
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-07-01 13:03:23 -0400

    Add webpack config & root component

---
+++ frontend/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Stock Tracker App: Phase Two</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
