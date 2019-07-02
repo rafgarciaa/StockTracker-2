46e9dce26e30803108163763a9d0bd81f7363246
Yu Qi Liu <yu.qi.liu236@gmail.com>
2019-07-02 11:53:00 -0400

    Setup .env for API token #GSM-22 added dotenv-webpack to hide api token in .env

--- frontend/util/apiUtil.js
+++ frontend/util/apiUtil.js
@@ -1,46 +1,47 @@
-export const API_KEY = 'Tpk_048c58eccfa04f9aae0310d105574605';
+// export const API_KEY = process.env.API_TOKEN;
+export const API_KEY = process.env.API_TEST_TOKEN;
 
 // filters
 export const newsFilters = [
-  'filter=datetime',
-  'headline',
-  'url',
-  'source',
+    'filter=datetime',
+    'headline',
+    'url',
+    'source',
 ].join(',');
 
 export const quoteFilters = [
-  'filter=symbol',
-  'companyName',
-  'open',
-  'close',
-  'high',
-  'low',
-  'latestPrice',
-  'latestVolume',
-  'avgTotalVolume',
-  'marketCap',
-  'peRatio',
-  'week52High',
-  'week52Low',
-  'change',
-  'changePercent',
+    'filter=symbol',
+    'companyName',
+    'open',
+    'close',
+    'high',
+    'low',
+    'latestPrice',
+    'latestVolume',
+    'avgTotalVolume',
+    'marketCap',
+    'peRatio',
+    'week52High',
+    'week52Low',
+    'change',
+    'changePercent',
 ].join(',');
 
 export const statsFilters = ['filter=dividendYield'].join(',');
 
 export const companyInfoFilters = [
-  'filter=description',
-  'exchange',
-  'sector',
-  'website',
-  'symbol',
-  'companyName',
+    'filter=description',
+    'exchange',
+    'sector',
+    'website',
+    'symbol',
+    'companyName',
 ].join(',');
 
 export const chartDataFilters = [
-  'filter=date',
-  'minute',
-  'label',
-  'close',
-  'average',
+    'filter=date',
+    'minute',
+    'label',
+    'close',
+    'average',
 ].join(',');
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: './frontend/stockTracker.js',
-  output: {
-    path: path.resolve(__dirname),
-    filename: './bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react'],
-          },
+module.exports = env => {
+    return {
+        entry: './frontend/stockTracker.js',
+        output: {
+            path: path.resolve(__dirname),
+            filename: './bundle.js',
         },
-      },
-    ],
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*'],
-  },
-};
+        module: {
+            rules: [{
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react'],
+                    },
+                },
+            }, ],
+        },
+        devtool: 'source-map',
+        resolve: {
+            extensions: ['.js', '.jsx', '*'],
+        },
+        plugins: [
+            new Dotenv()
+        ]
+    };
+}
