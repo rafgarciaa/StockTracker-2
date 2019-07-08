3768ee6db01a0392ea6a087c0f8a4914727d1d32
Rafael Garcia <rafael.garcia@weareadaptive.com>
2019-07-08 12:14:18 -0400

    #GSM-27 Add company info component

--- frontend/components/statsInfoPeers/statsInfoPeersLayout.tsx
+++ frontend/components/statsInfoPeers/statsInfoPeersLayout.tsx
@@ -1,15 +1,20 @@
 import React, { FunctionComponent } from 'react';
 import TopPeers from './topPeers';
+import CompanyInfo from './companyInfo';
+import { CompanyInfoState } from '../../utilities/interfaces';
 
 interface StatsInfoPeersProps {
+  companyInfo: CompanyInfoState;
   topPeers: string[];
 }
 
 const StatsInfoPeersLayout: FunctionComponent<StatsInfoPeersProps> = ({
   topPeers,
+  companyInfo,
 }) => {
   return (
     <div>
+      <CompanyInfo companyInfo={companyInfo} />
       <TopPeers topPeers={topPeers} />
     </div>
   );
