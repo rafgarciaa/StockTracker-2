9d2e57cd13286842fab19f69588fa6e297bf4af8
Rafael Garcia <rafael.garcia@weareadaptive.com>
2019-07-09 16:39:22 -0400

    #GSM-48 Styling for search bar area

--- frontend/components/header/priceOutput.tsx
+++ frontend/components/header/priceOutput.tsx
@@ -1,36 +1,36 @@
 import React, { FunctionComponent } from 'react';
 import { HeaderProps } from '../../utilities/interfaces';
 
+const changeToPercent = (num: number) => {
+  return num ? (num * 100).toFixed(2) : null;
+};
+
 const PriceOutput: FunctionComponent<HeaderProps> = ({
   latestPrice,
   change,
   changePercent,
 }) => {
   const isNegative = change <= 0;
-  const headerClassName = isNegative
-    ? 'header__price negative'
-    : 'header__price positive';
+  const headerClassName = isNegative ? 'negative' : 'positive';
   return (
-    <>
-      <div className={headerClassName}>
+    <div className="header__top-price">
+      <span>
+        <span className="icon--small">$</span>
+        {latestPrice != null ? Math.abs(latestPrice) : '0'}
+      </span>
+      <div className={`header__top-price--change ` + headerClassName}>
         <span>
-          <span className="icon--small">$</span>
-          {latestPrice != null ? Math.abs(latestPrice) : '0'}
+          <span className="icon--small">
+            {isNegative ? '\u2193' : '\u2191'}
+          </span>
+          {change != null ? Math.abs(change) : '0'}
         </span>
-        <div className="header__price--change">
-          <span>
-            <span className="icon--small">
-              {isNegative ? '\u2193' : '\u2191'}
-            </span>
-            {change != null ? Math.abs(change) : '0'}
-          </span>
-          <span>
-            {Math.abs(changePercent)}
-            <span className="icon--small">%</span>
-          </span>
-        </div>
+        <span>
+          {changeToPercent(Math.abs(changePercent))}
+          <span className="icon--small">%</span>
+        </span>
       </div>
-    </>
+    </div>
   );
 };
 
