71b418af76de0cce48cdeaa4ccf3137bc7aa5170
Yu Qi Liu <yu.qi.liu236@gmail.com>
2019-07-10 12:56:59 -0400

    fix merge conflict

--- frontend/components/chartNews/chart.tsx
+++ frontend/components/chartNews/chart.tsx
@@ -92,55 +92,10 @@ const Chart: FunctionComponent<ChartProps> = ({
         </a>
       </div>
 
-<<<<<<< HEAD
         {isFetchingChart ? (
           'LOADING...'
         ) : (
           <ResponsiveContainer width="100%" aspect={2}>
-            <AreaChart
-              data={displayedChartData}
-              margin={{
-                top: 0,
-                right: 0,
-                left: 0,
-                bottom: 0,
-              }}
-            >
-              <defs>
-                <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="15%" stopColor="#8884d8" stopOpacity={1} />
-                  <stop offset="85%" stopColor="#8884d8" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis
-                dataKey="dateTime"
-                interval={'preserveStart'}
-                tick={{ stroke: '#f4f6f9', strokeWidth: 0.1 }}
-              />
-              <YAxis
-                orientation="right"
-                domain={['dataMin', 'auto']}
-                tick={{ stroke: '#f4f6f9', strokeWidth: 0.1 }}
-              />
-              <Tooltip
-                cursor={{ stroke: 'red', strokeWidth: 2 }}
-                labelStyle={{ color: 'black' }}
-              />
-              <Area
-                type="monotone"
-                dataKey="price"
-                stroke="#8884d8"
-                fill="url(#colorPrice)"
-                fillOpacity={0.3}
-              />
-            </AreaChart>
-          </ResponsiveContainer>
-        )}
-      </div>
-=======
-      <ResponsiveContainer width="100%" aspect={2}>
         <AreaChart
           data={displayedChartData}
           margin={{
@@ -181,7 +136,8 @@ const Chart: FunctionComponent<ChartProps> = ({
           />
         </AreaChart>
       </ResponsiveContainer>
->>>>>>> 8babdd7d011e0cefc1bc32113e9257c08cd2b709
+        )}
+      </div>
     </div>
   );
 };
