8347a62214155246c1a24dd8d54dc3f3efbce588
Yu Qi Liu <yu.qi.liu236@gmail.com>
2019-07-10 17:24:03 -0400

    company overview and top peers loader #GSM-44

--- frontend/components/statsInfoPeers/topPeers.tsx
+++ frontend/components/statsInfoPeers/topPeers.tsx
@@ -1,18 +1,33 @@
 import React, { FunctionComponent } from 'react';
+import { FetchStatusElement } from '../../utilities/interfaces';
 
 interface TopPeersProps {
   topPeers: string[];
+  fetchingStatus: FetchStatusElement;
 }
 
-const TopPeers: FunctionComponent<TopPeersProps> = ({ topPeers }) => {
+const TopPeers: FunctionComponent<TopPeersProps> = ({
+  topPeers,
+  fetchingStatus,
+}) => {
   return (
     <div className="section-peers">
-      <h2 className="heading-section">Top Peers</h2>
-      {topPeers.map((peer, idx) => (
-        <span className="section-peers__peer" key={idx}>
-          {peer}
-        </span>
-      ))}
+      {fetchingStatus.startFetching && (
+        <div>
+          <h2 className="heading-section">Top Peers</h2>
+          {!fetchingStatus.doneFetching ? (
+            'LOADING...'
+          ) : (
+            <div>
+              {topPeers.map((peer, idx) => (
+                <span className="section-peers__peer" key={idx}>
+                  {peer}
+                </span>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
