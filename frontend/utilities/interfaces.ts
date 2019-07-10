328710b35aa97057491120b2a61376d7ffaf619b
Rafael Garcia <rafael.garcia@weareadaptive.com>
2019-07-10 16:54:26 -0400

    Fix typescript bug for headerprops

--- frontend/utilities/interfaces.ts
+++ frontend/utilities/interfaces.ts
@@ -110,6 +110,9 @@ export interface RootState {
 }
 
 export interface HeaderProps {
+  latestPrice?: number;
+  change?: number;
+  changePercent?: number;
   exchange?: string;
   sector?: string;
   updateTime?: string;
