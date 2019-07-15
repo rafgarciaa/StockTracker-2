f9179bec11d8ed3d6c2cdb505e9aad4f9f8b87db
Yu Qi Liu <yu.qi.liu236@gmail.com>
2019-07-15 15:02:48 -0400

    added error invalid api token #GSM-61

--- frontend/actions/quotesActions.ts
+++ frontend/actions/quotesActions.ts
@@ -58,10 +58,13 @@ const handleResponse = (response: {
   statusText: string;
   status: number;
 }) => {
+  console.log(response);
   if (response.status === 404) {
     throw new APIError('Company Not Found', response.status);
   } else if (response.status === 402) {
     throw new APIError('API Key Limit Reached', response.status);
+  } else if (response.status === 400) {
+    throw new APIError('Invalid API key', response.status);
   } else if (response.status !== 200) {
     throw new APIError(response.statusText, response.status);
   }
