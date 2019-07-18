e626cd43fc2dc80816f28f3afaec8e8c2cc4b55c
Yu Qi Liu <yu.qi.liu236@gmail.com>
2019-07-18 17:40:08 -0400

    gab DO NOT MERGE

--- frontend/reducers/quotes/favoritesReducer.ts
+++ frontend/reducers/quotes/favoritesReducer.ts
@@ -3,19 +3,28 @@ import { FavoriteState } from '../../utilities/interfaces';
 import { SetFavoritesAction } from '../../actions/quotesActions';
 import { Reducer, AnyAction } from 'redux';
 
+export const REDUCERS = {
+  [QUOTES_ACTION_TYPES.SET_FAVORITES]: setFavoritesReducer,
+};
+
+function setFavoritesReducer(state = {}, action: SetFavoritesAction) {
+  return {
+    ...state,
+    [action.payload.symbol]: action.payload,
+  };
+}
+
 const favoriteReducer: Reducer<FavoriteState, AnyAction> = (
   state: FavoriteState = {},
   action: AnyAction
 ) => {
-  switch (action.type) {
-    case QUOTES_ACTION_TYPES.SET_FAVORITES:
-      return {
-        ...state,
-        [action.payload.symbol]: action.payload,
-      };
-    default:
-      return state;
+  const innerReducer = REDUCERS[action.type];
+
+  if (innerReducer) {
+    return innerReducer(state, action);
   }
+
+  return state;
 };
 
 export default favoriteReducer;
