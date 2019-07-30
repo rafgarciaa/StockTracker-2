81a1adc7142d8b334f0b7647132195761e0a4324
Vincent Boivin <vboivin04@gmail.com>
2019-07-30 09:47:22 -0400

    Update updateTimeReducer.ts

--- frontend/reducers/updates/updateTimeReducer.ts
+++ frontend/reducers/updates/updateTimeReducer.ts
@@ -1,6 +1,9 @@
 import { UPDATE_ACTION_TYPES } from '../../constants/updateTypes';
 import { UpdateTypes } from '../../actions/updateActions';
 
+// Same comment as here : 
+// https://github.com/Vboivin/StockTracker-2/blob/master/frontend/components/statsInfoPeers/topPeersContainer.tsx
+
 const updateTimeReducer = (state: string = '', action: UpdateTypes) => {
   switch (action.type) {
     case UPDATE_ACTION_TYPES.SET_UPDATE_TIME:
