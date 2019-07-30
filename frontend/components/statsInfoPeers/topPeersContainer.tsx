09c5f6461be654467dcca77c1ca6e25c20f4959b
Vincent Boivin <vboivin04@gmail.com>
2019-07-30 09:32:04 -0400

    Update topPeersContainer.tsx

--- frontend/components/statsInfoPeers/topPeersContainer.tsx
+++ frontend/components/statsInfoPeers/topPeersContainer.tsx
@@ -3,6 +3,18 @@ import TopPeers from './topPeers';
 import { RootState } from '../../utilities/interfaces';
 import { selectFetchingStatus } from '../../utilities/selectors';
 
+// You could destructure the state object in the 
+// function's signature to get rid of some
+// boilerplate.
+
+// For example (state: RootState) would become
+// ({quotes,fetchStatus}: RootState)
+// then you could map topPeers to
+// quotes.topPeers.
+
+// It is not a necessary change, it comes
+// down to preference.
+
 const mapStateToProps = (state: RootState) => ({
   topPeers: state.quotes.topPeers,
   fetchStatus: state.fetchStatus.topPeers,
