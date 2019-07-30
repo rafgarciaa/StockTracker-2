5db209b7359d160ccf68c41af6903c2490d3dd91
Vincent Boivin <vboivin04@gmail.com>
2019-07-30 09:37:00 -0400

    Update getCurrentDate.ts

--- frontend/utilities/getCurrentDate.ts
+++ frontend/utilities/getCurrentDate.ts
@@ -1,4 +1,9 @@
 export const getCurrentDate = () => {
   const currentDate: Date = new Date();
+  
+  // You could use string templating if you feel like it.
+  // It would look like this : `currentDate.toDateString() currentDate.toLocaleTimeString()`.
+  // It is not a necessary change, it comes down to preference.
+  
   return currentDate.toDateString() + ' ' + currentDate.toLocaleTimeString();
 };
