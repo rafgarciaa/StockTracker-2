72746d96f23facb713afd8c5ec4cf98c1a4223d5
Vincent Boivin <vboivin04@gmail.com>
2019-07-30 09:50:51 -0400

    Update companyNamesReducer.ts

--- frontend/reducers/quotes/companyNamesReducer.ts
+++ frontend/reducers/quotes/companyNamesReducer.ts
@@ -2,6 +2,9 @@ import { QUOTES_ACTION_TYPES } from '../../constants/actionTypes';
 import { ActionsTypes } from '../../actions/quotesActions';
 import { CompanyNameState } from '../../utilities/interfaces';
 
+// Same comment as here :
+// https://github.com/Vboivin/StockTracker-2/blob/master/frontend/components/statsInfoPeers/topPeersContainer.tsx
+
 const companyNamesReducer = (
   state: CompanyNameState[] = [],
   action: ActionsTypes
